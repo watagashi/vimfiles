2bb9389288e4e49faf82841d922132e25314ae0d
Takashi WADA <twada@amy.hi-ho.ne.jp>
2016-03-10 19:33:35 +0900

    Update Mercurial repository

--- pathogenup.cmd
+++ pathogenup.cmd
@@ -1,8 +1,21 @@
+@rem vim:ts=4 sw=4 et
 @for /d %%d in ("%~dp0bundle\*") do @(
-        if exist %%d\.git (
-            pushd %%d
-            git remote -v
-            git pull
-	    popd
-            )
-        )
+
+    rem Git
+
+    if exist %%d\.git (
+        pushd %%d
+        git remote -v
+        git pull
+        popd
+    )
+
+    rem Mercurial
+
+    if exist %%d\.hg (
+        pushd %%d
+        hg path default
+        hg pull -u
+        popd
+    )
+)
