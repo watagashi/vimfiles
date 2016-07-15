3a2bc897bebdd7b40fbe28a5e897311f653f5ded
WADA Takashi <350677+watagashi@users.noreply.github.com>
2016-07-15 16:15:55 +0900

    installvim: install from local archive

--- installvim.cmd
+++ installvim.cmd
@@ -3,13 +3,14 @@ setlocal
 
 set UNZIP="%ProgramFiles%\7-Zip\7z" x
 set ARCHIVE="%TEMP%\vim.zip"
+if not "%1"=="" set ARCHIVE=%1
 set DEST="%ProgramFiles%"
 
 set SOURCE=http://files.kaoriya.net/goto/vim74w32
 if %PROCESSOR_ARCHITECTURE%==AMD64 set source=http://files.kaoriya.net/goto/vim74w64
 
-curl --location --output %ARCHIVE% %SOURCE%
+if "%1"=="" curl --location --output %ARCHIVE% %SOURCE%
 %UNZIP% %ARCHIVE% -o%DEST% -y
-del %ARCHIVE%
+if not "%1"=="" del %ARCHIVE%
 
 endlocal
