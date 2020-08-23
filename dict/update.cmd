da0fe8df4ca04917a563be3d37d6acfc6b356118
WADA Takashi <twada@amy.hi-ho.ne.jp>
2020-08-24 02:04:47 +0900

    Update multiple types of SKK-JISYO

--- dict/update.cmd
+++ dict/update.cmd
@@ -1,1 +1,9 @@
-curl -Lo %~dp0\SKK-JISYO.L http://openlab.ring.gr.jp/skk/skk/dic/SKK-JISYO.L
+@pushd %~dp0
+@echo L > dic.txt
+@echo geo >> dic.txt
+@echo zipcode >> dic.txt
+@echo office.zipcode >> dic.txt
+@echo jinmei >> dic.txt
+@for /f %%t in (dic.txt) do curl -Lo SKK-JISYO.%%t http://openlab.ring.gr.jp/skk/skk/dic/SKK-JISYO.%%t
+@del dic.txt
+@popd
