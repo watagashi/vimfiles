29c350580295d91c42883f09e6fb0374f3117973
WADA Takashi <twada@amy.hi-ho.ne.jp>
2024-12-18 18:05:51 +0900

    dict/update: Change source URL of zipcode

--- dict/update.cmd
+++ dict/update.cmd
@@ -1,9 +1,10 @@
 @pushd %~dp0
 @echo L > dic.txt
 @echo geo >> dic.txt
-@echo zipcode >> dic.txt
-@echo office.zipcode >> dic.txt
 @echo jinmei >> dic.txt
 @for /f %%t in (dic.txt) do curl -Lo SKK-JISYO.%%t http://openlab.ring.gr.jp/skk/skk/dic/SKK-JISYO.%%t
+@echo zipcode > dic.txt
+@echo office.zipcode >> dic.txt
+@for /f %%t in (dic.txt) do curl -Lo SKK-JISYO.%%t http://openlab.ring.gr.jp/skk/skk/dic/zipcode/SKK-JISYO.%%t
 @del dic.txt
 @popd
